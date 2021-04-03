da117e97e769d1d1a59c7da143e26b1615bff6c4
tangmf <tangmf@91160.com>
2021-04-03 17:34:32 +0800

    2021/04/03 config 代码

---
+++ cloud-config-client-3355/src/main/java/tate/springcloud/ConfigClient3355.java
@@ -0,0 +1,18 @@
+package tate.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+/**
+ * @Author tangmf
+ * @Date 2021/4/3 4:36 下午
+ * @Description
+ */
+@SpringBootApplication
+@EnableEurekaClient
+public class ConfigClient3355 {
+    public static void main(String[] args) {
+        SpringApplication.run(ConfigClient3355.class, args);
+    }
+}
