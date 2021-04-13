c63ed73d1c92b66b004e993e91a0c5e136d749d0
tangmf <tangmf@91160.com>
2021-04-13 20:48:33 +0800

    2021/04/13 stream 代码

---
+++ cloud-stream-rabbitmq-consumer8803/src/main/java/com/tate/springcloud/StreamMQMain8803.java
@@ -0,0 +1,16 @@
+package com.tate.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @Author tangmf
+ * @Date 2021/4/13 8:10 下午
+ * @Description
+ */
+@SpringBootApplication
+public class StreamMQMain8803 {
+    public static void main(String[] args) {
+        SpringApplication.run(StreamMQMain8803.class, args);
+    }
+}
